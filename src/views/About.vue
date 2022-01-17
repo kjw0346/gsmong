787ff699b1aa145fadd6b5744e6595052f5475a9
김진우 <kjw0346@naver.com>
2022-01-17 17:26:16 +0900

    정보

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "About.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/AboutWebsite.vue
@@ -0,0 +1,23 @@
+<template>
+<v-container class="nfpage">
+    <v-card max-width="600px" class="card">
+        <v-card-title>소개</v-card-title>
+        <v-card-text class="text--primary">
+            <div>분당구 정자동 초 중 고 수학의 지름길 학원입니다.<br>수학단과 심화학습 관리,<br>개인별 맞춤교육.질의,<br>문답형 학습자중심학습법, <br>플립러닝 메타 인지학습법.<br>수학에 흥미와 자신감을 기르고 수학공부가 즐거워하는 분위기로 내신 up!!<br>수능1등급 목표.  .</div>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn to="/">홈</v-btn>
+        </v-card-actions>
+    </v-card>
+</v-container>
+</template>
+
+<script>
+export default {
+name: "About.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
